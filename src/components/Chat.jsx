7b2be7e38209f730d8749a60b4755268a821ad59
nicky-t <nickytaketo0903@gmail.com>
2020-05-30 19:48:48 +0900

    チャットテキスト表示、画像表示

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,25 @@
 import React from "react";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import Avatar from "@material-ui/core/Avatar";
+import profileNo from "../assets/img/no-profile.png";
+import profileImage from "../assets/img/torahack.png";
 
-const Chat = () => {};
+const Chat = (props) => {
+  const isQuestion = props.type === "question";
+  const classes = isQuestion ? "p-chat__row" : "p-chat__reverse";
+  return (
+    <ListItem>
+      <ListItemAvatar>
+        {isQuestion ? (
+          <Avatar alt="icon" src={profileImage} />
+        ) : (
+          <Avatar alt="icon" src={profileNo} />
+        )}
+      </ListItemAvatar>
+      <div className="p-chat__bubble">{props.text}</div>
+    </ListItem>
+  );
+};
 
 export default Chat;
